7bd8e6ecb636422f7853f4456e22eea2207363e7
Nunzio Lanza <lanza.nunzio@gmail.com>
2024-01-17 17:01:25 -0500

    Added picture of car and some light theming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Make sure this points to your actual CSS file
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Nudelhaus Racing</h1>
       </header>
+      <main className="App-main">
+        <h2>Welcome to Nudelhaus Racing</h2>
+        <p>Join us in our journey of racing, restoration, and passion.</p>
+        {/* Update the src attribute of the img tag as shown below */}
+        <img src={`${process.env.PUBLIC_URL}/car.jpg`} alt="Nudelhaus Racing Car" className="App-car-image" />
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2024 Nudelhaus Racing. All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
